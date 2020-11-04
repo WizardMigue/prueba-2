ac1aace92e8688b3ad355532c4fb72f964ca15cc
Miguel <208609@iessierradeguara.com>
2020-11-04 17:15:31 +0100

    javascript

---
+++ web js1/app.js
@@ -0,0 +1,67 @@
+// alert("Hola Mundo");
+
+// // Tipos de una línea
+// // Tipos de datos
+// // Tipo cadena de texto o string
+// "Esto es una cadena de texto"
+// 'Esto tambíen es una cadena de texto'
+// "I'm the teacher"
+// 'I\'m de teacher'
+
+// // alert("Esto es una cadena de texto");
+// // alert('Esto tambíen es una cadena de texto');
+// // alert("I'm the teacher");
+// // alert('I\'m de teacher');
+
+// // Tipo número entero
+// // Se pueden usar operafortes aritméticos
+// // alert("2"+2);
+// // alert(6-5);
+// // alert(3*2);
+// // alert(2/8);
+
+
+// // Tipo número decimal
+// // Se pueden usar operafortes aritméticos
+// // alert(2.5+2.9);
+// // alert(6.9-5.3);
+// // alert(3.5*2.5);
+// // alert(2.1/8.7);
+
+// // Tipo booleano
+// // True False
+// // Operaciones lógicas &&  ||
+// alert(true && true);
+// alert(false || true);
+
+// let mensaje = "Hola Mundo";
+// let nombre = "Miguel";
+// alert("hola " + nombre);
+// alert(mensaje);
+// console.log(mensaje)
+// let numero = 3;
+// numero = 3 + 2;
+// numero++;
+// alert(numero);
+// numero--;
+// alert(numero);
+
+
+// let va1 = 5;
+// let va2 = 3;
+// let resultado = va1 * va2;
+// alert(resultado);
+
+alert("Mira el h1")
+let elemento = document.getElementById("encabezado");
+elemento.innerHTML="Hola clase";
+let resuelto;
+
+resuelto = confirm("¿Quieres que ter salude?");
+console.log(resuelto);
+
+if (resuelto == true) {
+    alert("Hola");
+}
+
+alert("Fin del programa");
