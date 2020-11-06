74086f410d5cb087bce6f9eb52699dc409fd9395
Miguel <208609@iessierradeguara.com>
2020-11-06 20:58:07 +0100

    Adivina el numero

--- web js1/app.js
+++ web js1/app.js
@@ -34,34 +34,83 @@
 // alert(true && true);
 // alert(false || true);
 
-// let mensaje = "Hola Mundo";
-// let nombre = "Miguel";
-// alert("hola " + nombre);
-// alert(mensaje);
-// console.log(mensaje)
-// let numero = 3;
-// numero = 3 + 2;
-// numero++;
-// alert(numero);
-// numero--;
-// alert(numero);
+let mensaje = "Hola Mundo";
+let nombre = "Miguel";
+alert("hola " + nombre);
+alert(mensaje);
+console.log(mensaje)
+let numero = 3;
+numero = 3 + 2;
+numero++;
+alert(numero);
+numero--;
+alert(numero);
 
 
-// let va1 = 5;
-// let va2 = 3;
-// let resultado = va1 * va2;
-// alert(resultado);
+let va1 = 5;
+let va2 = 3;
+let resultado = va1 * va2;
+alert(resultado);
 
 alert("Mira el h1")
 let elemento = document.getElementById("encabezado");
-elemento.innerHTML="Hola clase";
+elemento.innerHTML = "Hola clase";
 let resuelto;
 
 resuelto = confirm("¿Quieres que ter salude?");
 console.log(resuelto);
-
 if (resuelto == true) {
     alert("Hola");
 }
-
+else {
+    alert("Hola mundo")
+}
 alert("Fin del programa");
+
+let incognita = "3"; // este es el número que hayq eu adivinar
+let respuesta; // aquí guardaremos lo que introduzca el usuario
+respuesta = prompt("Escribe un número del 1 al 10");
+if (respuesta == null) {
+    alert("Fin del Juego, le has dado a 'Cancelar' ")
+}
+else if (respuesta == incognita) {
+    alert("Enhorabuena, has ganado")
+}
+else if (respuesta == "1") {
+    alert("Uy!. Dale a F5 y prueba otra vez")
+}
+else if (respuesta == "2") {
+    alert("Uy!. Dale a F5 y prueba otra vez")
+}
+else if (respuesta == "4"|| respuesta=="2") {
+    alert("Uy!. Dale a F5 y prueba otra vez")
+}
+else if (respuesta == "5") {
+    alert("Uy!. Dale a F5 y prueba otra vez")
+}
+else if (respuesta == "6") {
+    alert("Uy!. Dale a F5 y prueba otra vez")
+}
+else if (respuesta == "7") {
+    alert("Uy!. Dale a F5 y prueba otra vez")
+}
+else {
+    alert("Lo siento, no ha podido ser. Dale a F5 para jugar otra vez")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
