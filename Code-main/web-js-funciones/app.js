01b1401ce385aecd1145202c921bcb38571021bc
Miguel <208609@iessierradeguara.com>
2020-11-18 15:40:34 +0100

    Code

---
+++ Code-main/web-js-funciones/app.js
@@ -0,0 +1,13 @@
+function cambiarParrafo(contenido) {
+    let parrafo = document.getElementById("parrafo").innerHTML = contenido;
+}
+function cambiarParrafo(IDparrafo, contparrafo) {
+    let parrafo = document.getElementById(IDparrafo).innerHTML = contparrafo;
+}
+cambiarParrafo("parrafo","Hola caracola");
+
+document.getElementById("cara")
+value = "hola";
+
+ 
+
---
+++ WEB-JS-ARRAYS-OBJETS/app.js
@@ -0,0 +1,20 @@
+let alumnoArray = ["Pepito", "Perez", 54]
+console.log(alumnoArray[0]);
+
+console.error(alumnoArray[1]);
+
+alert(alumnoArray[2]);
+
+console.log(alumnoArray.length)
+
+let alumnoObjeto = {
+    "nombre": "Pepito",
+    "apellido": "Perez",
+    "edad": 54
+};
+
+console.log(alumnoObjeto.nombre);
+
+console.error(alumnoObjeto.apellido);
+
+alert(alumnoObjeto.edad);
